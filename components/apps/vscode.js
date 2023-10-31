072fcd56c16f6ffd8ca15836cbf3970cf1daa7f7
dev.j <jde.to>
2023-10-31 00:29:55 -0500

    update

--- components/apps/vscode.js
+++ components/apps/vscode.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function VsCode() {
   return (
     <iframe
-      src="https://github.com/FrostSnow0219/ubuntu"
+      src="https://github.com/FrostSnow0219/main"
       frameBorder="0"
       title="VsCode"
       className="h-full w-full bg-ub-cool-grey"
